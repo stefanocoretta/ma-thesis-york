a653c85e337c1ac9881ce796e0e143b7b4f79d04
Stefano Coretta <stefanoc.social@me.com>
2017-01-27 16:09:12 +0000

    add backup script

---
+++ backup.bash
@@ -0,0 +1,7 @@
+rsync -anv --no-whole-file --exclude '*.DS_Store' \
+    ~/Documents/GitHub/ma-thesis-york \
+    /Volumes/Multimedia/
+
+rsync -avz --no-whole-file --exclude '*.DS_Store' \
+    ~/Documents/GitHub/ma-thesis-york \
+    /Volumes/Multimedia/
