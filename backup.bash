3521434830707acee043f2ceca8dbafb4f2cf212
Stefano Coretta <stefanoc.social@me.com>
2017-01-28 16:32:56 +0000

    add pull

--- backup.bash
+++ backup.bash
@@ -1,3 +1,5 @@
+## Push to external hard drive
+
 rsync -anv --no-whole-file --exclude '*.DS_Store' \
     ~/Documents/GitHub/ma-thesis-york \
     /Volumes/Multimedia/
@@ -5,3 +7,13 @@ rsync -anv --no-whole-file --exclude '*.DS_Store' \
 rsync -avz --no-whole-file --exclude '*.DS_Store' \
     ~/Documents/GitHub/ma-thesis-york \
     /Volumes/Multimedia/
+
+## Pull data from external hard drive
+
+rsync -anv --no-whole-file --exclude '*.DS_Store' \
+    /Volumes/Multimedia/ma-thesis-york/data \
+    ~/Documents/GitHub/ma-thesis-york/
+
+rsync -avz --no-whole-file --exclude '*.DS_Store' \
+    /Volumes/Multimedia/ma-thesis-york/data \
+    ~/Documents/GitHub/ma-thesis-york/
