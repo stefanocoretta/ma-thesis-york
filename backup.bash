3c35633568eed7c479b85b7108798389171fdbd2
Stefano Coretta <stefanoc.social@me.com>
2017-02-01 09:43:03 +0000

    add delete files

--- backup.bash
+++ backup.bash
@@ -1,10 +1,10 @@
 ## Push to external hard drive
 
-rsync -anv --no-whole-file --exclude '*.DS_Store' \
+rsync -anv --no-whole-file --exclude '*.DS_Store' --delete \
     ~/Documents/GitHub/ma-thesis-york \
     /Volumes/Multimedia/
 
-rsync -avz --no-whole-file --exclude '*.DS_Store' \
+rsync -avz --no-whole-file --delete --exclude '*.DS_Store' \
     ~/Documents/GitHub/ma-thesis-york \
     /Volumes/Multimedia/
 
