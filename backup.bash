9d17f00f8c08ef5d120e4f61b31fb947eb0a2a9f
Stefano Coretta <stefanoc.social@me.com>
2019-07-25 10:36:42 +0200

    :bug: copy only content of data/ dir

--- backup.bash
+++ backup.bash
@@ -1,19 +1,19 @@
 ## Push to external hard drive
 
 rsync -anv --no-whole-file --exclude '*.DS_Store' --delete \
-    ~/GitHub/ma-thesis-york/data \
+    ~/GitHub/ma-thesis-york/data/ \
     /Volumes/Seagate/language-data/ma-thesis-york/data
 
 rsync -avz --no-whole-file --exclude '*.DS_Store' --delete \
-    ~/GitHub/ma-thesis-york/data \
+    ~/GitHub/ma-thesis-york/data/ \
     /Volumes/Seagate/language-data/ma-thesis-york/data
 
 ## Pull data from external hard drive
 
 rsync -anv --no-whole-file --exclude '*.DS_Store' \
-    /Volumes/Seagate/language-data/ma-thesis-york/data \
+    /Volumes/Seagate/language-data/ma-thesis-york/data/ \
     ~/GitHub/ma-thesis-york/data
 
 rsync -avz --no-whole-file --exclude '*.DS_Store' \
-    /Volumes/Seagate/language-data/ma-thesis-york/data \
+    /Volumes/Seagate/language-data/ma-thesis-york/data/ \
     ~/GitHub/ma-thesis-york/data
